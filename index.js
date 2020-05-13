ff5dd3ff889ec84e3e2550a7957b2844211cf59e
mattcbodily <mattcbodily@gmail.com>
2020-05-13 15:03:49 -0600

    added more customization options

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-module.exports = function easyAlert(message, height, width){
+module.exports = function easyAlert(message, height, width, background, fontColor, borderWidth, borderRadius){
     const customAlert = document.createElement('section');
     customAlert.innerText = message;
-    customAlert.style = `height: ${height}px; width: ${width}px;`
+    customAlert.style = `
+        height: ${height}px; 
+        width: ${width}px;
+        background: ${background};
+        color: ${fontColor};
+        border: ${borderWidth};
+        border-radius: ${borderRadius} 
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        margin-top: calc(${height}px / -2);
+        margin-left: calc(${width}px / -2);`
 
     document.body.appendChild(customAlert);
 }
