302d813e1bda4de152b703a111d162ddaca4875b
mattcbodily <mattcbodily@gmail.com>
2020-05-14 15:38:51 -0600

    setting up pre-built alerts

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-module.exports = function easyAlert(message, height, width, background, fontColor, borderWidth, borderRadius, top, left){
-    const customAlert = document.createElement('section');
+module.exports = {
+    customAlert: function(message, height, width, background, fontColor, borderWidth, borderRadius, top, left){
+        const customAlert = document.createElement('section');
+    
+        customAlert.innerText = message;
+        customAlert.style = `
+            height: ${height}px; 
+            width: ${width}px;
+            background: ${background};
+            color: ${fontColor};
+            border: ${borderWidth}px;
+            border-radius: ${borderRadius};
+            position: fixed;
+            top: ${top};
+            left: ${left};`
+    
+        document.body.appendChild(customAlert);
+    },
+    textAlert: function(){
 
-    customAlert.innerText = message;
-    customAlert.style = `
-        height: ${height}px; 
-        width: ${width}px;
-        background: ${background};
-        color: ${fontColor};
-        border: ${borderWidth}px;
-        border-radius: ${borderRadius};
-        position: fixed;
-        top: ${top};
-        left: ${left};`
+    },
+    imageAlert: function(){
 
-    document.body.appendChild(customAlert);
+    },
+    imageTextAlert: function(){
+        
+    }
 }
