a636e7a6058453d4b89079d55b78f7d0b2dee047
mattcbodily <mattcbodily@gmail.com>
2020-05-14 13:23:28 -0600

    basic custom alert works

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = function easyAlert(message, height, width, background, fontColor, borderWidth, borderRadius, top, left){
     const customAlert = document.createElement('section');
-    const App = document.querySelector('.App');
 
     customAlert.innerText = message;
     customAlert.style = `
@@ -14,6 +13,5 @@ module.exports = function easyAlert(message, height, width, background, fontColo
         top: ${top};
         left: ${left};`
 
-    // App.appendChild(customAlert);
     document.body.appendChild(customAlert);
 }
