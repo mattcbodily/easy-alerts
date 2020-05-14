b6d3eef191698055547c9539408ec4480b9fb605
mattcbodily <mattcbodily@gmail.com>
2020-05-14 15:45:27 -0600

    added basic text alert styling

--- index.js
+++ index.js
@@ -16,13 +16,38 @@ module.exports = {
     
         document.body.appendChild(customAlert);
     },
-    textAlert: function(){
+    textAlert: function(alertObj){
+        const textAlert = document.createElement('section');
 
+        textAlert.innerText = alertObj.message;
+        textAlert.style = `
+            height: 50px;
+            width: 250px;
+            box-sizing: border-box;
+            padding: 0px 10px;
+            border: 1px solid black;
+            border-radius: 5px;
+            background-color: ${alertObj.backgroundColor};
+            color: white;
+            font-size: 18px;
+            font-family: Arial, Helvetica, sans-serif;
+            line-height: 50px;
+            position: relative;`
+        
+        const close = document.createElement('span');
+
+        close.innerText('x');
+        close.style = `
+            position: absolute;
+            top: 0px;
+            right: 5px;
+            line-height: 20px;
+            cursor: pointer;`
     },
     imageAlert: function(){
 
     },
     imageTextAlert: function(){
-        
+
     }
 }
